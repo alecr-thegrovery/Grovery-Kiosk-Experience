86765857695bd838d93035767dceba75bad861da
Alec Reimel <alecreimel1@gmail.com>
2023-03-15 12:26:07 -0400

    QRCode tooltips pt1

--- components/ContentComponents/QRCodePreset/QRCodePreset.js
+++ components/ContentComponents/QRCodePreset/QRCodePreset.js
@@ -1,7 +1,8 @@
 /*===== Components =====*/
-import React from "react"
+import React, { useState, useEffect } from 'react';
 import ZoomIcon from '@components/SVGComponents/ZoomIcon'
 import QRCode from "react-qr-code";
+import tippy from 'tippy.js';
 
 /*===== Styles =====*/
 import componentStyles from './styles.module.scss'
@@ -13,6 +14,25 @@ export default function QRCodePreset({
 }) {
 
   //console.log(author);
+  useEffect(() => {
+    //JS GOES HERE
+    tippy('[data-tippy-content]', {
+      placement: 'right',
+      arrow: true,
+      theme: 'light',
+      animation: 'fade',
+      show: true,
+    });
+  /*  let tippyVal = 
+    tippy('[data-tippy]', {
+      content: tippyVal,
+      //trigger: 'manual',
+      placement: 'right',
+    });*/
+
+    /*const instance = tippy(document.querySelector('[data-tippy-content]'));
+    instance.show();*/
+  });
 
   return (
     <div className={componentStyles.QRCodePreset}>
@@ -22,6 +42,8 @@ export default function QRCodePreset({
           bgColor="#ffffff"
           fgColor="#212121"
           style={{ height: "auto", width: "8vw" }}
+          data-tippy-content={toolTipText}
+          
         />
     </div>
   )
