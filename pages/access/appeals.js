1444d8b94d702146125c56f516b7eebb97cc3f48
Alec Reimel <alecreimel1@gmail.com>
2023-03-16 21:53:01 -0400

    appeals page & related assets updated

--- pages/access/appeals.js
+++ pages/access/appeals.js
@@ -17,7 +17,7 @@ import QRCodePreset from "@components/ContentComponents/QRCodePreset";
 import utilStyles from '@styles/utils.module.css'
 import pageStyles from '@styles/pages.module.scss'
 
-let title = "Using the My BMS Cases Provider Portal";
+let title = "Prior Authorization & Appeals Support";
 //{["Using the My BMS Cases ", <strong>Provider Portal</strong>]}
 let footerText = "";
 
@@ -31,7 +31,7 @@ export default function Post({ postData }) {
       <div className={pageStyles.pageLayout}>
         {/*Header*/}
         <DynamicHeader
-          heading={["Using the My BMS Cases ", <strong>Provider Portal</strong>]}
+          heading={["Prior Authorization & Appeals Support"]}
         />
 
         {/*Content*/}
@@ -44,30 +44,47 @@ export default function Post({ postData }) {
                 {/*LEFT CELL*/}
                 <div className={pageStyles.cell}>
                   
-                  <BlockQuote icon="/images/icons/contentComputerScreen.svg">
-                    <p>This secure provider portal offers convenient, on-demand access to patient reimbursement cases for select BMS medications.</p>
+                  <BlockQuote icon="/images/icons/contentListIcon.svg">
+                  <h2>Prior Authorization (PA)</h2>
+                  <h2>If required in the Benefits Review, our team can:</h2>
+                  <ul>
+                    <li>Help obtain plan-specific PA forms and understand PA requirements</li>
+                    <li>Track PAs and follow up with payer</li>
+                  </ul>
                   </BlockQuote>
 
-                  <h2>The provider portal allows you to: </h2>
+                  <h2>Or you can obtain payer requirement information through the BMS Access Map at BMSAccessSupport.com:</h2>
                   <ul>
-                    <li>Access case status updates </li>
-                    <li>Enroll in co-pay program and view details, including eligibility dates, annual benefit cap, and available balance</li>
-                    <li>Submit and track reverification status </li>
-                    <li>Use e-signature for simplified processing </li>
-                    <li>Securely message your regional Patient Access Specialist</li>
+                    <li>Get on-demand access to coverage policies and formulary status for select BMS medications within local health plans</li>
+                    <li>View utilization management criteria, including PA and documentation requirements, diagnostic testing information, etc.</li>
                   </ul>
-                  <br/>
-                  <QRCodePreset 
-                    link="https://www.youtube.com/watch?v=nM4okRvCg2g"
-                    toolTipText="Scan to visit Provider Portal"
-                    toolTipPlacement="right"
-                  />
+
+                  <BlockQuote icon="/images/icons/contentClipboardIcon.svg">
+                  <h2>Appeals Support</h2>
+                  <p>Get helpful information, forms, and other resources regarding the appeals process, including:</p>
+                  </BlockQuote>
+                  
+                  <ul>
+                    <li>Medical necessity letter template and checklist</li>
+                    <li>Claim denial letters template</li>
+                    <li>Appeals letter template</li>
+                    <li>CMS forms and documents</li>
+                    <li>
+                      <a href="#">Tap to learn more about the provider portal</a>
+                    </li>
+                  </ul>
                   
                 </div>               
 
                 {/*RIGHT CELL*/}
                 <div className={pageStyles.cell} data-centered>
-                  <img src="/images/content/provider-portal-img.png" alt="" />
+                  <img src="/images/content/appeals-img.png" alt="" />
+                  <p>To access the BMS Access Payer Map, scan the QR code below and select the appropriate medication.</p>
+                  <QRCodePreset 
+                    link="https://www.youtube.com/watch?v=nM4okRvCg2g"
+                    toolTipText="Scan to visit BMS Access Payer Map"
+                    toolTipPlacement="right"
+                  />
                 </div>
 
               </div>
