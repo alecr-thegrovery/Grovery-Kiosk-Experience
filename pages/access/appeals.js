9f7bac912e907eba68d400b832e5aa63b735ce8f
Alec Reimel <alecreimel1@gmail.com>
2023-04-28 12:39:49 -0400

    Access pages content updated

--- pages/access/appeals.js
+++ pages/access/appeals.js
@@ -18,7 +18,7 @@ import QRCodePreset from "@components/ContentComponents/QRCodePreset";
 import utilStyles from '@styles/utils.module.css'
 import pageStyles from '@styles/pages.module.scss'
 
-let title = "Prior Authorization & Appeals Support";
+let title = "PA & Appeals Support";
 //{["Using the My BMS Cases ", <strong>Provider Portal</strong>]}
 let footerText = "";
 
@@ -47,34 +47,35 @@ export default function Post({ postData }) {
                 <div className={pageStyles.cell}>
                   
                   <BlockQuote icon="/images/icons/contentListIcon.svg">
-                  <h2>Prior Authorization (PA)</h2>
+                  {/*<h2>Prior Authorization (PA)</h2>*/}
                   <h2>If required in the Benefits Review, our team can:</h2>
                   <ul>
-                    <li>Help obtain plan-specific PA forms and understand PA requirements</li>
+                    <li>Help obtain plan-specific PA forms and understand PA requirements </li>
                     <li>Track PAs and follow up with payer</li>
                   </ul>
                   </BlockQuote>
 
-                  <h2>Or you can obtain payer requirement information through the BMS Access Map at BMSAccessSupport.com:</h2>
+                  <h2>Or you can obtain payer requirement information through the BMS Access Payer Map at BMSAccessMap.com:</h2>
                   <ul>
                     <li>Get on-demand access to coverage policies and formulary status for select BMS medications within local health plans</li>
-                    <li>View utilization management criteria, including PA and documentation requirements, diagnostic testing information, etc.</li>
+                    <li>View utilization management criteria, including Prior Authorization and documentation requirements, diagnostic testing information, etc.</li>
                   </ul>
 
                   <BlockQuote icon="/images/icons/contentClipboardIcon.svg">
-                  <h2>Appeals Support</h2>
-                  <p>Get helpful information, forms, and other resources regarding the appeals process, including:</p>
+                    <h2>Appeals Support </h2>
+                    <p>Get helpful information, forms, and other resources regarding the appeals process, including:</p>
+                    <ul>
+                      <li>Medical necessity letter template and checklist</li>
+                      <li>Claim denial letters template</li>
+                      <li>Appeals letter template</li>
+                      <li>CMS forms and documents</li>
+                      <li>
+                        <a href="/enrollment/provider-portal">Tap to learn more about the provider portal</a>
+                      </li>
+                    </ul>
                   </BlockQuote>
                   
-                  <ul>
-                    <li>Medical necessity letter template and checklist</li>
-                    <li>Claim denial letters template</li>
-                    <li>Appeals letter template</li>
-                    <li>CMS forms and documents</li>
-                    <li>
-                      <a href="#">Tap to learn more about the provider portal</a>
-                    </li>
-                  </ul>
+                  
                   
                 </div>               
 
@@ -85,6 +86,15 @@ export default function Post({ postData }) {
                   <QRCodePreset 
                     link="https://www.bmsaccessmap.com/opdivo?utm_source=bms_congress_access_app&utm_medium=qr_code_scan&utm_campaign=congress_access_event"
                     toolTipText="Scan to visit BMS Access Payer Map"
+                    toolTipPlacement="right"
+                  />
+
+                  <br/><br/>
+
+                  <img src="/images/content/pa-appeals-supporting.png" alt="" />
+                  <QRCodePreset 
+                    link="https://www.bmsaccesssupport.bmscustomerconnect.com/forms-resources?utm_source=bms_congress_access_app&utm_medium=qr_code_scan&utm_campaign=congress_access_event"
+                    toolTipText="Scan for PA &amp; Appeals Forms &amp; Resources"
                     toolTipPlacement="right"
                   />
                 </div>
