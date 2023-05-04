42661833c44b3368b5adccfcc3418d3f2cae29b5
Alec Reimel <alecreimel1@gmail.com>
2023-05-04 18:53:11 -0400

    rollback

--- components/SVGComponents/ComputerFrame/Frame4.js
+++ components/SVGComponents/ComputerFrame/Frame4.js
@@ -43,7 +43,7 @@ export default function Frame4({
           </g>
         </g>
         
-        <foreignObject className={componentStyles.content} x="0" y="0" width="calc(100% - 100px);" height="calc(100% - 100px);">
+        <foreignObject className={componentStyles.content} x="0" y="0" width="100%" height="100%">
           <div className={componentStyles.contentInner} data-overflow-detect>
             {children}
           </div>
