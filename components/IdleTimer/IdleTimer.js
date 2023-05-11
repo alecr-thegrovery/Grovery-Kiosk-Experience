c0470c4df8f1004064989fe413d9fa2ead0a7bf6
Alec Reimel <alecreimel1@gmail.com>
2023-05-11 14:03:30 -0400

    reset full timer

--- components/IdleTimer/IdleTimer.js
+++ components/IdleTimer/IdleTimer.js
@@ -14,8 +14,8 @@ import componentStyles from './styles.module.scss'
 let redirectUrl = "/?idle"  //test setting, replace
 
 //set time in minutes
-//let timeout = 2.5 //total time
-let timeout = 0.75  //shortened for testing
+let timeout = 2.5 //total time
+//let timeout = 0.75  //shortened for testing
 let promptBeforeIdle = 0.5 //time left in prompt
 
 //convert minutes to milliseconds
